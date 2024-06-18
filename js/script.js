66532cf3a8dce7f52cb2c8a11d7fe82491d2e5e0
instanick <nickgomesdeoliveira200816@gmail.com>
2024-06-18 17:31:12 -0300

    add js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// constante
+const fotogrande = document.getElementById('fotogrande')
+const desc = document.getElementById('desc')
+    // função para trocar a foto
+function trocaimg(value) {
+    fotogrande.src = 'assets/' + value
+}
